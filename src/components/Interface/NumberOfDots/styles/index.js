2917819a432f4900bd324a8d9ae23553e02ed467
Dillon <dpett122@gmail.com>
2019-06-12 15:29:58 -0400

    fix(style): fix input styling

--- src/components/Interface/NumberOfDots/styles/index.js
+++ src/components/Interface/NumberOfDots/styles/index.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const StyledForm = styled.form`
     display: flex;
     flex-direction: column;
-    width: 200px;
+    width: 250px;
     justify-content: space-between;
 
     div {
